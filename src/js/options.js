078e1df85ac234aac364be6afdd18c39919efd61
Bryan Lilly <bryanjameslilly@gmail.com>
2021-08-29 16:20:41 +0700

    Did some code revisions - got user settings to save and be applied - working on option.html layout and design

--- src/js/options.js
+++ src/js/options.js
@@ -5,15 +5,10 @@ const saveBtn = document.getElementById('save-btn');
 saveBtn.addEventListener('click', () => {
 
     if(document.getElementById('show_btns').checked){
-        console.log('true');
-        chrome.storage.sync.set({ btn_disp: true });
-        
+        chrome.storage.sync.set({ btn_disp: true });   
     } else {
-        console.log('false');
         chrome.storage.sync.set({ btn_disp: false });
-        
     }
-
 });
 
 // chrome.storage.sync.get({})
